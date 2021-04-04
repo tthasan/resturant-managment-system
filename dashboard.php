1a27ea178d443024d86e127befa121810114ed82
Mahmudul Hasan <mahmudul.mh80@gmail.com>
2021-04-05 01:11:51 +0600

    manu calculation added

--- dashboard.php
+++ dashboard.php
@@ -28,6 +28,30 @@ if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     if ( $row['password'] == $_POST['password']) {
         echo "Welcome ".$_POST['username']."!";
+
+        ?>
+        <form action="calculation.php" method="POST">
+
+            <br>
+            <input type="checkbox" id="soup" name="soup" value="soup" /> <label for="soup">Soup</label><br>
+            <input type="checkbox" id="onthon" name="onthon" value="onthon" /> <label for="onthon">Onthon</label> <br> 
+            <input type="checkbox" id="pakura" name="pakura" value="pakura" /> <label for="soup">Pakura</label> <br> 
+            <br>
+            <input type="checkbox" id="rice" name="rice" value="rice" /> <label for="Rice">Rice</label><br>
+            <input type="checkbox" id="nan" name="nan" value="nan" /> <label for="nan">nan</label><br>
+            <input type="checkbox" id="nodols" name="nodols" value="nodols" /> <label for="nodols">nodols</label><br>
+            <br>
+            <input type="checkbox" id="beef" name="beef" value="beef" /> <label for="beef">beef</label><br>
+            <input type="checkbox" id="chicken" name="chicken" value="chicken" /> <label for="chicken">chicken</label><br>
+            <input type="checkbox" id="mutton" name="mutton" value="mutton" /> <label for="mutton">mutton</label><br>
+            <br>
+            <input type="checkbox" id="yogart" name="yogart" value="yogart" /> <label for="yogart">yogart</label><br>
+            <input type="checkbox" id="sweet" name="sweet" value="sweet" /> <label for="sweet">Sweet</label><br>
+            <input type="checkbox" id="drinks" name="drinks" value="drinks" /> <label for="drinks">drinks</label><br>
+            <input type="Submit" value="Submit"/> 
+        </form>
+        <?php
+
     }
     else {
         echo "You have put a wrong password, please <a href='index.html'>try again</a>.";
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
+<?php
+
+
+
+$str1 = "Welcome Resturant Management system. <br>"; 
+$str2 = "Welcome to our Application <br>"; 
+$str3 = "Welcome please login before you order. <br>;";
+
+echo $str2;
+
+?>
+
 <form action="dashboard.php" method="post">
 	<div class="container">
 	  <input type="text" placeholder="Enter Username" name="username" required>
 	  <input type="password" placeholder="Enter Password" name="password" required>
 	  <button type="submit">Login</button>
 	</div>
-  </form>
+</form>
