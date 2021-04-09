1ac51346cb082c6c276782c2aa4f6bd8a5e8454b
Rafaf Tahsin <rafaftahsin@gmail.com>
2021-04-09 23:48:24 +0600

    added space after back button

--- dashboard.php
+++ dashboard.php
@@ -69,7 +69,7 @@ if (isset($_SESSION['username'])) {
                 <!-- <label for="quantity">Quantity (between 1 and 5):</label> -->
                 <input class="form-control" type="number" id="quantity" name="quantity" min="1" max="5">
                 <br>
-                <a href="index.php">Back</a>
+                <a href="index.php">Back&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
                 <input type="submit" class="btn btn-primary" value="Confirm Order"/>
             </form> 
         </div>
